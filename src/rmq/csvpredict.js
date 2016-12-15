97d4576950ed0fa03402ff1f1e78e8876d45d28e
seveibar <seveibar@gmail.com>
2016-12-15 13:30:01 -0500

    cleaned csv prediction

--- src/rmq/csvpredict.js
+++ src/rmq/csvpredict.js
@@ -1,12 +1,11 @@
-//@flow
+// @flow
 import { rpc } from './util';
 
-export default async function csvpredict(surveycsv: string){
-  let prediction = await rpc('predictor_queue', surveycsv);
-  try{
+export default async function csvpredict(surveycsv: string): Promise<string> {
+  const prediction = await rpc('predictor_queue', surveycsv);
+  try {
     return prediction;
-  }catch(e){
-    console.log("Could not parse prediction: ", prediction);
-    return {};
+  } catch (e) {
+    throw new Error('could not parse prediction');
   }
 }
